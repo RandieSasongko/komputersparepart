197d7c0c7abd8fcbef3c7a1c93820cb16d952656
RandieSasongko <randie.sasongko08@mhs.mdp.ac.id>
2023-05-17 09:05:01 +0700

    17/05/2023 9.04

--- app/src/main/java/com/if4a/kulinerkita/Activity/TambahActivity.java
+++ app/src/main/java/com/if4a/kulinerkita/Activity/TambahActivity.java
@@ -3,14 +3,55 @@ package com.if4a.kulinerkita.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.if4a.kulinerkita.R;
 
 public class TambahActivity extends AppCompatActivity {
 
+    private EditText etNama, etAsal, etDeskripsiSingkat;
+    private Button btnSimpan;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tambah);
+
+        etNama = findViewById(R.id.et_nama);
+        etAsal = findViewById(R.id.et_asal);
+        etDeskripsiSingkat = findViewById(R.id.et_deskripsi_singkat);
+        btnSimpan = findViewById(R.id.btn_tambah);
+
+        btnSimpan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nama, asal, deskripsiSingkat;
+
+                nama = etNama.getText().toString();
+                asal = etAsal.getText().toString();
+                deskripsiSingkat = etDeskripsiSingkat.getText().toString();
+
+                if(nama.trim().isEmpty())
+                {
+                    etNama.setError("Nama Tidak Boleh Kosong");
+                }
+                else if(asal.trim().isEmpty())
+                {
+                    etAsal.setError("Asal Tidak Boleh Kosong");
+                }
+                else if(deskripsiSingkat.trim().isEmpty())
+                {
+                    etDeskripsiSingkat.setError("Deskripsi Singkat Tidak Boleh Kosong");
+                }
+                else
+                {
+                    
+                }
+
+            }
+        });
+
     }
 }
